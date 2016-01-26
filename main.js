1a3da69ce455a9dad4cea6ce6dbdc8e0dbe2970f
David Niederhauser <niederhauser.dave@gmail.com>
2016-01-26 12:59:06 -0800

    finish model; start view

--- main.js
+++ main.js
@@ -12,6 +12,22 @@ var board = [
 ];
 
 // 1. START THE GAME
+
+//    inputs:
+// - currentPlayer, board, won
+
+
+//   outcome:
+// - board is empty
+var startGame = function() {
+  won = false;
+  currentPlayer = "X";
+  board =
+ ["","","",
+  "","","",
+  "","",""
+ ];
+};
 // 2. MOVE (CHANGES TURN)
 // inputs:
 // -whose turn it is,
@@ -38,4 +54,28 @@ if (gameWon()) {
   }
 };
 // 3. WIN THE GAME
-var gameWon = function (){};
+
+//inputs:
+// -board
+
+//outputs:
+//-true or false
+
+var gameWon = function (){
+  if (
+  ((board[0] === board[1]) && (board[0]=== board[2]) && board[0] !=="") ||
+  ((board[3] === board[4]) && (board[3]=== board[5]) && board[3] !=="") ||
+  ((board[6] === board[7]) && (board[6]=== board[8]) && board[6] !=="")
+  ){
+    return true;
+  } else{
+  return false;
+ }
+};
+
+
+/* RENDER OUR VIEW */
+var render = function () {
+
+};
+
